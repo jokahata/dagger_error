9f9b8695198dd1a742f29468c0961f6f12087d3a
John Okahata <jokahata@users.noreply.github.com>
2018-03-03 12:15:44 -0800

    Pressing enter echoes the contents of the text area

--- js/dagger.js
+++ js/dagger.js
@@ -1,1 +1,22 @@
-console.log("hello");
+$(document).ready(function(){
+    $('#errorlog').keypress(function(e){
+        if(e.which == 13){
+            clear_output();
+            var errorLogLines = $('textarea#errorlog').val().split('\n');
+            var outputLines = $('#errorfound');
+            for (var i = 0; i < errorLogLines.length; i++) {
+                var line = errorLogLines[i];
+                appendParagraph(outputLines, line);
+            }
+        }
+    });
+});
+
+function clear_output() {
+    var outputLines = $('#errorfound');
+    outputLines.children().remove();
+}
+
+function appendParagraph(parent, toAppend) {
+    parent.append("<p>" + toAppend + "</p>");
+}
